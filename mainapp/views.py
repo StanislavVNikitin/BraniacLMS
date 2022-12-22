6ce46d8318d9fb6dda99382af39b16baf5bead43
StanislavVNikitin <stasvnikitin@mail.ru>
2022-12-22 18:41:15 +0000

    lesson_3 Templates including

--- mainapp/views.py
+++ mainapp/views.py
@@ -11,11 +11,8 @@ class NewsPageView(TemplateView):
     def get_context_data(sekf, **kwargs):
         context = super().get_context_data(**kwargs)
         context["news_title"] = "Громкий новостной заголовок"
-        context[
-            "news_preview"
-            ] = "Предварительное описание, которое заинтересует каждого"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
         return context
-
 
 
 class CoursesPageView(TemplateView):
